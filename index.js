4ef249ffaca177ce56fe819a999f6dd835841241
shubham khandal <shubhamshubhamkhandal@outlook.com>
2020-10-25 13:11:54 +0530

    index page

---
+++ index.js
@@ -0,0 +1,6 @@
+import { render } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App/>, document.getElementById('root'));
